914b9450b08a5247a75637c9b9be65d5b9398506
amal salim <amal.salim@gmail.com>
2022-11-18 15:17:27 -0800

    with mongoose

--- server.js
+++ server.js
@@ -5,6 +5,27 @@ const app = express();
 //Setting PORT to 5000 if PORT is not listed in environmental variables.
 const PORT = process.env.PORT || 5000;
 
+const mongoose = require('mongoose');
+
+// Gets the Username and Password
+const MONGO_URI = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@mongo:27017`;
+
+// Creating the connect function
+const connectDB = async () => {
+  await mongoose
+    .connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("Mongo connected successfully"))// Logs out successful when MongoDB connects.
+    .catch((e) => {
+      console.log(e.message);// Logs out the error message if it encounters any.
+    });
+};
+
+// Calling the Connect Function
+connectDB();
+
 // Creating the `GET` route
 app.get("/", (req, res) => {
   res.send("<h1>Welcome Friends</h1>");
